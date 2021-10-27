6cf1eef8cdb6fcd6d1f182d7832c2d6d90f28c11
Jorgin <me@jorge.com>
2021-10-27 09:21:44 -0300

    ✨ feat

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 import { next, prev, dots } from "./scripts/carousel.js";
 import { InitItems } from "./scripts/init-items.js";
 
+
+const $cartButton = document.querySelector('header .cart');
+const $cart = document.querySelector('main .cart');
+const $closeCart = $cart.querySelector('div button.close-cart');
 const $next = document.querySelector('main section.carousel div.buttons-slide .buttons button.next');
 const $prev = document.querySelector('main section.carousel div.buttons-slide .buttons button.prev');
 const $dots = document.querySelectorAll('main section.carousel div.buttons-slide div.dots div');
@@ -10,3 +14,7 @@ $prev.addEventListener('click', () => {prev($prev)});
 dots($dots);
 
 InitItems()
+
+$cartButton.addEventListener('click', () => $cart.classList.add('active'));
+
+$closeCart.addEventListener('click', () => $cart.classList.remove('active'));
